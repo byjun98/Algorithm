a282ce884027f68fd63b5509618f83ca979e80e9
byjun98 <156162788+byjun98@users.noreply.github.com>
2025-08-14 14:54:01 +0900

    [D4] Title: [S/W 문제해결 기본] 6일차 - 계산기1, Time: 56 ms, Memory: 50,176 KB -BaekjoonHub

--- SWEA/D4/1222. ［S／W 문제해결 기본］ 6일차 － 계산기1/［S／W 문제해결 기본］ 6일차 － 계산기1.py
+++ SWEA/D4/1222. ［S／W 문제해결 기본］ 6일차 － 계산기1/［S／W 문제해결 기본］ 6일차 － 계산기1.py
@@ -1,11 +1,29 @@
 T = 10
 for t in range(1, T + 1):
-    N = int(input())
-    arr = input().rstrip()
-    calc = 0
-    for i in range(N):
-        if arr[i] == '+':
-            continue
-        else: calc += int(arr[i])
+    n = int(input())
+    infix = input().strip()
 
-    print(f"#{t} {calc}")
+    # 1. 중위  후위 변환
+    postfix = ""
+    stack = []
+    for ch in infix:
+        if ch.isdigit():
+            postfix += ch
+        else:  # '+'
+            stack.append(ch)
+    # 스택에 남은 연산자 추가
+    while stack:
+        postfix += stack.pop()
+
+    # 2. 후위식 계산
+    calc_stack = []
+    for ch in postfix:
+        if ch.isdigit():
+            calc_stack.append(int(ch))
+        else:  # '+'
+            b = calc_stack.pop()
+            a = calc_stack.pop()
+            calc_stack.append(a + b)
+
+    result = calc_stack[0]
+    print(f"#{t} {result}")
