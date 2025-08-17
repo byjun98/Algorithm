740cb8133255eb9bab842994995e3d334f6d099a
byjun98 <156162788+byjun98@users.noreply.github.com>
2025-08-17 19:16:21 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,15 +1,12 @@
 tall = [int(input()) for _ in range(9)]
-total = sum(tall)
-
-found = False
-for i in range(9):
+all_tall = sum(tall)
+for i in range(8):
     for j in range(i+1, 9):
-        # 두 명을 제외했을 때 합이 100이 되면
-        if total - tall[i] - tall[j] == 100:
-            result = [tall[k] for k in range(9) if k != i and k != j]
-            for h in sorted(result):
+        if all_tall - (tall[i] + tall[j]) == 100:
+            fake1, fake2 = tall[i], tall[j]
+            tall.remove(fake1)
+            tall.remove(fake2)
+            tall.sort()
+            for h in tall:
                 print(h)
-            found = True
-            break
-    if found:
-        break
+            exit()  # 바로 종료
