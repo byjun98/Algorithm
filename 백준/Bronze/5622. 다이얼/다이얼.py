f26f14279e727a448c9515090c0e9ada140a079d
byjun98 <156162788+byjun98@users.noreply.github.com>
2025-09-06 00:38:29 +0900

    [Bronze II] Title: 다이얼, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -0,0 +1,22 @@
+al = list(input())
+sum = 0
+for i in range(len(al)):
+    if al[i] in 'ABC':
+        sum += 3
+    elif al[i] in 'DEF':
+        sum += 4
+    elif al[i] in 'GHI':
+        sum += 5
+    elif al[i] in 'JKL':
+        sum += 6
+    elif al[i] in 'MNO':
+        sum += 7
+    elif al[i] in 'PQRS':
+        sum += 8
+    elif al[i] in 'TUV':
+        sum += 9
+    elif al[i] in 'WXYZ':
+        sum += 10
+    else:
+        sum += 2
+print(sum)
