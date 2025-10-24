5f4f68ba7681cb0dbb936e0e49c6df154a4b9a96
byjun98 <156162788+byjun98@users.noreply.github.com>
2025-10-24 17:17:00 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 404 ms, Memory: 185268 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,13 +1,21 @@
 import sys
 input = sys.stdin.readline
 
-S = list(input().strip())
+S = input().strip()
 q = int(input())
-al = [0] * 26
-for i in range(q):
-    cnt = 0
-    cl = list(input().split())
-    for j in range(int(cl[1]), int(cl[2])+1):
-        if ord(S[j]) == ord(cl[0]):
-            cnt += 1
-    print(cnt)
+
+prefix = [[0] * 26 for _ in range(len(S) + 1)]
+
+for i, ch in enumerate(S, 1):
+    for k in range(26):
+        prefix[i][k] = prefix[i - 1][k]
+    prefix[i][ord(ch) - 97] += 1
+
+out = []
+for _ in range(q):
+    a, l, r = input().split()
+    l, r = int(l), int(r)
+    idx = ord(a) - 97
+    out.append(str(prefix[r + 1][idx] - prefix[l][idx]))
+
+print('\n'.join(out))
